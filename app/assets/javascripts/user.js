c45c4da325cf092f736516fe9cebc931f41b09cd
keisuke.goda <keisuke8925gdk@gmail.com>
2018-11-11 18:01:57 +0900

    雑多な修正

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -21,7 +21,7 @@ $(function() {
   var add_member = $("#chat-group-users")
 
   $(".chat-group-form__input").on("keyup", function() {
-    var input = $("#user-search-field").val();
+    var input = $(this).val();
 
     $.ajax({
       type: 'GET',
@@ -30,31 +30,32 @@ $(function() {
       dataType: 'json',
     })
 
-   .done(function(users) {
-     $("#user-search-result").empty();
-     if (users.length !== 0) {
-       users.forEach(function(user){
-         appendUser(user);
-       });
-     }
-     else {
-       appendNoUser("一致するユーザはいません");
-     }
-    })
-   .fail(function(){
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser("一致するユーザはいません");
+      }
+     })
+
+    .fail(function(){
     alert('ユーザー検索に失敗しました')
-   })
+    })
   });
 
 ////ユーザ追加の実装
-function appendMember(user) {
-  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                <input name='group[user_ids][]' type='hidden' value='${ user.userId }'>
-                <p class='chat-group-user__name'>${ user.userName }</p>
-                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-              </div>`
-    add_member.append(html);
-}
+  function appendMember(user) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user.userId }'>
+                  <p class='chat-group-user__name'>${ user.userName }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+      add_member.append(html);
+  }
 
   $(document).on("click", ".chat-group-user__btn--add", function() {
     var user = $(this).data();
