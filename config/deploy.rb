022b68694d22903bea46c7be22dc7a18c329e06c
keisuke.goda <keisuke8925gdk@gmail.com>
2018-11-25 21:58:11 +0900

    デプロイ諸々

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,36 +4,34 @@ lock "~> 3.11.0"
 set :application, "my_app_name"
 set :repo_url, "git@example.com:me/my_repo.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+lock '<Capistranoのバージョン>'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :application, 'chat-space'
+set :repo_url,  'https://github.com/Keisukegdk/chat-space.git'
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
 
-# Default value for :pty is false
-# set :pty, true
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/test.pem']
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+end
